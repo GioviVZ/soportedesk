3ec85986023dfe761dbf6a654f222d457e356ccd
GioviVZ <gvivancoz97@gmail.com>
2026-06-14 22:34:00 -0500

    chore: scaffold Spring Boot backend project

---
+++ soportedesk-backend/src/main/java/com/inia/soportedesk/SoportedeskApplication.java
@@ -0,0 +1,11 @@
+package com.inia.soportedesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoportedeskApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SoportedeskApplication.class, args);
+    }
+}
